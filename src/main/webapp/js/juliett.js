670309935ba58e597e86d8b73d15adfede7a8cff
Jack Lowrie <jblowrie@u.northwestern.edu>
2014-05-22 14:30:11 -0500

    cleaned up juliet.js
    — removed test function
    
    —clarified comments and reformatted to make navigating the document
    easier

--- src/main/webapp/js/juliett.js
+++ src/main/webapp/js/juliett.js
@@ -1,38 +1,32 @@
-$(function() {
-  $("#nametest").click(function() {
-    var testdata = {
-        username: $('#username').val(),
-    };
-    $.post(URL, testdata, function(data) {
-       console.log(data);
-    });
-  });
-});
-
 //student class stores all info about a student using the service except course preferences
-function student(name, residence, starttime, endtime) {
-	this.name = name;
-	this.residence = residence;
-	this.starttime = starttime;
-	this.endtime = endtime;
-}
+	function student(name, residence, starttime, endtime) {
+		this.name = name;
+		this.residence = residence;
+		this.starttime = starttime;
+		this.endtime = endtime;
+	}
+
 //methods to set properties of class student
-function setName (newName){
-	this.name = newName;
-}
-function setResidence (newRes){
-	this.residence = newRes;
-}
-function setStart(newStart){
-	this.starttime = newStart;
-}
-function setEnd(newEnd){
-	this.endtime = newEnd;
-}
+	function setName (newName){
+		this.name = newName;
+	}
+
+	function setResidence (newRes){
+		this.residence = newRes;
+	}
+
+	function setStart(newStart){
+		this.starttime = newStart;
+	}
+
+	function setEnd(newEnd){
+		this.endtime = newEnd;
+	}
 
 //creates the user object when the form is submitted
-var user = new student() // should initialize to "null" values for error catching
-$("#userinfo").submit(function(){
-	user.setName(getElementById("username"))
-	user.
-});
+	var user = new student() // should initialize to "null" values for error catching
+	$("#userinfo").submit(function(){
+		user.setName(getElementById("username"))
+		//set residence var
+		//set start/end time vars
+	});
