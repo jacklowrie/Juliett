8606c52047d1e445c10299023dea35831e536388
Jack Lowrie <jblowrie@u.northwestern.edu>
2014-05-22 13:46:13 -0500

    Started creating user object

--- src/main/webapp/js/juliett.js
+++ src/main/webapp/js/juliett.js
@@ -8,3 +8,14 @@ $(function() {
     });
   });
 });
+
+function student(name, residence, starttime, endtime) {
+	this.name = name;
+	this.residence = residence;
+	this.starttime = starttime;
+	this.endtime = endtime;
+}
+
+$("#userinfo").submit(function(){
+	var user = new student();
+});
