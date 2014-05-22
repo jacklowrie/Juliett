efc7c1401c923949d2e9f1c31c5e772995336571
Jack Lowrie <jblowrie@u.northwestern.edu>
2014-05-22 14:21:23 -0500

    continuation of last commit

--- src/main/webapp/js/juliett.js
+++ src/main/webapp/js/juliett.js
@@ -9,13 +9,30 @@ $(function() {
   });
 });
 
+//student class stores all info about a student using the service except course preferences
 function student(name, residence, starttime, endtime) {
 	this.name = name;
 	this.residence = residence;
 	this.starttime = starttime;
 	this.endtime = endtime;
 }
+//methods to set properties of class student
+function setName (newName){
+	this.name = newName;
+}
+function setResidence (newRes){
+	this.residence = newRes;
+}
+function setStart(newStart){
+	this.starttime = newStart;
+}
+function setEnd(newEnd){
+	this.endtime = newEnd;
+}
 
+//creates the user object when the form is submitted
+var user = new student() // should initialize to "null" values for error catching
 $("#userinfo").submit(function(){
-	var user = new student();
+	user.setName(getElementById("username"))
+	user.
 });
