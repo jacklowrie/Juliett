13832400485e28aabe16ab8c5d55b1cd76695303
zhaoyangliu <liuzy.bupt@gmail.com>
2014-06-04 19:00:02 -0500

    just found that I did push all process part codes

---
+++ src/main/java/WEBINF/classes/team_juliett_processor/ScheduleChecker.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team_juliett_processor;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+/**
+ *
+ * @author Jeremymidvidy
+ */
+public class ScheduleChecker {
+
+    private schedule possibleSchedules;
+
+    public ScheduleChecker(schedule s)
+    {
+        possibleSchedules = s;
+    }
+
+    ////////////////===========================REMOVE CONFLICTS MAIN METHOD=====================/////////////////
+
+    public schedule[] resolveConflicts()
+    {
+
+       //Get the two arrays of courses for the resolveConflicts() helper methods
+       Course[] l1 = possibleSchedules.getMandCourses();
+       Course[] l2 = possibleSchedules.getOptCourses();
+       
+       
+
+       //~~Make all the possible combinations of each list~~//
+       ArrayList<ArrayList<Course>> mandList = makeCombinations(l1);
+       ArrayList<ArrayList<Course>> optList = makeCombinations(l2);
+
+       //remove conflicts from each individual list
+       mandList = removeConflicts(mandList);
+       optList = removeConflicts(optList);
+
+       //Make each combination of mandatory classes with every possible combinations of additional classes//
+       ArrayList<ArrayList<Course>> allSchedulesList = makeFullList(mandList, optList);
+
+       //Remove all combinations that are not four in length
+       allSchedulesList = shortenList(allSchedulesList);
+
+       //Then remove conflicts from the total list
+       allSchedulesList = removeConflicts(allSchedulesList);
+
+       //Convert from ArrayList to an arrayof Schedules//
+       schedule allSchedules = new schedule(allSchedulesList, l1, l2);
+       schedule[] aS = allSchedules.getAllSchedules();
+     
+
+       //this is an array of schedules
+       return aS;
+
+    }
+
+    ////////////========================= HELPER METHODS ================================/////
+
+    //returns true if the two courses conflict
+    private boolean conflicts(Course c1, Course c2)
+    {
+       if(c1.conflict(c2) == true)
+           return true;
+       else
+           return false;
+
+    }
+    
+    ///Removes the conflicts from each ArrayList element passed in the entire ArrayList<ArrayList<>> Param
+    public ArrayList<ArrayList<Course>> removeConflicts(ArrayList<ArrayList<Course>> list)
+        {
+        boolean[] switches = new boolean[list.size()];
+        int boolCounter = 0;
+        boolean done = false;
+        for(ArrayList<Course> temp: list)
+        {
+
+            for(int k = 0; k < temp.size() && done == false; k++)
+            {
+                for(int i = k+1; i <= temp.size()-1 && done == false; i++)
+                {
+                     if(conflict(temp.get(k), temp.get(i)))
+                    {
+                         switches[boolCounter] = true;
+                         done = true;
+                    }
+                }
+            }
+            done = false;
+            boolCounter++;
+
+        }
+
+        //find the indexes of the switches
+        ArrayList<Integer> indexOfConflicts = new ArrayList<Integer>();
+        for(int i = 0; i < switches.length; i++)
+        {
+            if(switches[i] == false)
+                indexOfConflicts.add(i); //add the indexes of valid conflicts to the list
+        }
+
+        //make a new list of only valid combinations from the indexes of the valid switches
+        ArrayList<ArrayList<Course>> validList = new ArrayList<ArrayList<Course>>();
+        for(int validIndex : indexOfConflicts)
+            {
+                validList.add(list.get(validIndex));
+            }
+
+        return validList;
+    }
+
+    //Shortens the list of l1 to a specficies value
+    public static ArrayList<ArrayList<Course>> shortenList(ArrayList<ArrayList<Course>> l1)
+    {
+        ArrayList<Integer> validIndexes = new ArrayList<Integer>();
+        int place = 0;
+        for(ArrayList<Course> temp : l1)
+        {
+            if(temp.size() == 4)
+                validIndexes.add(place);
+            place++;
+        }
+
+        ArrayList<ArrayList<Course>> newList = new ArrayList<ArrayList<Course>>();
+        for(int i : validIndexes)
+        {
+            newList.add(l1.get(i));
+        }
+        return newList;
+    }
+
+    //Returns true if Course a and Course c have conflicts
+    public boolean conflict(Course a, Course c)
+    {
+        for (int i = 0; i <5; i++){
+            if (a.get_days_of_week()[i] && c.get_days_of_week()[i])
+                if ((a.start_time == c.start_time) || (a.start_time < c.end_time && a.start_time > c.start_time)
+                                                      || (a.end_time > c.start_time && a.end_time < c.end_time))
+                    return true;
+        }
+        return false;
+    }
+
+
+    //takein the Course[] and outputs the ArrayList<ArrayList<Course>> where every ArrayList<Course> element
+    //is one of the combinations
+    private ArrayList<ArrayList<Course>> makeCombinations(Course[] l1)
+    {
+       ArrayList<ArrayList<Course>> mainList = new ArrayList<ArrayList<Course>>();
+
+       //[A, B, C, D, E]
+       int outerStart = 0;
+       while(outerStart < l1.length)
+       {
+        ArrayList<Course> t = new ArrayList<Course>();
+        t.add(l1[outerStart]);
+        mainList.add(t);
+        for(int firstAdd = outerStart; firstAdd < l1.length; firstAdd++)
+         {
+           int innerStart = firstAdd +1;
+           ArrayList<Course> temp = new ArrayList<Course>();
+           for(int i = innerStart; i < l1.length; i++)
+                {
+                    temp.add(l1[outerStart]);
+                    for(int j = innerStart; j <=i; j++)
+                        temp.add(l1[j]);
+                    mainList.add(temp);
+                    temp = new ArrayList<Course>();
+                }
+         }
+
+          outerStart++;
+       }
+       return mainList;
+    }
+
+    //makes every possible combination of courses
+    public ArrayList<ArrayList<Course>> makeFullList(ArrayList<ArrayList<Course>> mandList, ArrayList<ArrayList<Course>> optList)
+    {
+       ArrayList<ArrayList<Course>> fullList = new ArrayList<ArrayList<Course>>();
+       //each m is one of the possible list of mandatory classes
+       //each o is one of the possible lists of optional classes
+       //to make every possible combination:
+       //each m gets every possible o stored in a new list
+       for(ArrayList<Course> m: mandList)
+       {
+           for(ArrayList<Course> o: optList)
+           {
+               ArrayList<Course> temp = new ArrayList<Course>(m); //each temp contains a the next list of mandatory classes from mandList
+               for(Course k: o){
+                 temp.add(k); }//add the current OptList to temp
+               fullList.add(temp); //add the current temp to the full list and then move the next optLists
+            }
+
+        }
+       return fullList;
+    }
+}
---
+++ src/main/java/WEBINF/classes/team_juliett_processor/ScheduleGenerator.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team_juliett_processor;
+
+/**
+ *
+ * @author Jeremymidvidy
+ */
+public class ScheduleGenerator 
+{
+    //a list of schedules that are can feasibly made
+    private schedule[] validSchedules;
+    private int rating;
+    private schedule bestSchedule;
+    private User bob;
+    private Course[] mands;
+    private Course[] opts;
+    //Initalizes the list of schedules that must be passed from the main function
+    public ScheduleGenerator(schedule[] VS, User b, Course[] m, Course[] o)
+    {
+        validSchedules = VS;
+        rating = 0;
+        bestSchedule = null;
+        bob = b;
+        mands = m;
+        opts = o;
+    }
+     
+    //this is the main function
+    public schedule getBestSchedule()
+    {
+        if(bestSchedule == null)
+            RankSchedules(validSchedules); //internal call to the rankSchedules method
+        return bestSchedule;
+    }
+    
+    private void RankSchedules(schedule[] s)
+    {
+        
+        //Create local variables for this method
+        schedule[] poss = validSchedules;
+        
+        //Rank each schedule indivudally
+        for(int i = 0; i < poss.length; i++)
+        {
+            int rank = rateSchedule(poss[i]);
+            poss[i].setRank(rank);
+         }
+        
+        //Find the highest rank schedule the list based on rank
+        int highestRank = 0;
+        int bestIndex = 0;
+        for(int i = 0; i < poss.length; i++)
+        {
+            if(poss[i].getRank() > highestRank)
+                bestIndex = i;
+        }
+        
+        //set bestSchedule to the bestRank
+        bestSchedule = poss[bestIndex];
+    }
+    
+    public int rateSchedule(schedule s)
+    {
+        //Three sets of local variables for this method
+        int scheduleScore;
+        int scheduleSt;
+        int scheduleEt;
+        Course lastCourseOfDay = null;
+        Course firstCourseOfDay = null;
+        
+        Course[] a = s.getAllCourses();
+       
+        //Sort the courses in chronilogical order
+        sortByTime(a);
+        
+        //Initialize the local variables with the sorted array of courses
+        int userStartTime = bob.getStartTime();
+        int userEndTime = bob.getEndTime();
+        
+        scheduleSt = a[0].start_time;
+        firstCourseOfDay = a[0];
+        
+        scheduleEt = a[a.length-1].end_time;
+        lastCourseOfDay = a[a.length-1];
+        
+        //change schedule score based on the number of mandatory courses in the schedule - most significant
+        rankOnNumOfMandatoryClasses(s);
+        
+        //change scheduleScore based on the start and end time prefrences - middle significant
+        rankOnTimePrefrences(userStartTime, userEndTime, scheduleSt, scheduleEt, s);
+        
+        //change the schedule score based on the locations of each class - least significant
+        rankOnLocation(s);
+        
+        return s.scheduleScore;
+    }
+    
+    private void rankOnNumOfMandatoryClasses(schedule a)
+    {
+        int numberofMands = 0;
+        Course[] allCourses = a.getAllCourses();
+        numberofMands = howManyMands(allCourses);
+        if (numberofMands == 1)
+            a.scheduleScore += 10;
+        else if(numberofMands == 2)
+            a.scheduleScore += 20;
+        else if(numberofMands == 3)
+            a.scheduleScore += 30;
+        else if(numberofMands == 4)
+            a.scheduleScore += 40;
+        else
+            a.scheduleScore -= 10;
+    }
+    
+    private void rankOnTimePrefrences(int uS, int uE, int sS, int sE, schedule a)
+    {
+        //now adjust the schedule scores based on time conflicts and prefrences
+        if(sS >= uS && sE <= uE)
+            a.scheduleScore += 10;
+        else if(sS < uS && sE <= uE)
+            a.scheduleScore += 5;
+        else if(sS >= uS && sE > uE)
+            a.scheduleScore += 5;
+        else
+            a.scheduleScore += -5;
+    }
+       
+  
+    
+    private void rankOnLocation(schedule s)
+    {
+        int totalRank = 0;
+        Course[] a = s.getAllCourses();
+        
+        //compare the location of the first class to where they live on campus
+        //high ranking because most important
+        if(a[0].getLocation() == bob.getLoc())
+            totalRank+=5;
+        else
+            totalRank-=5;
+        
+        //change totalRank based on the location of the next classes
+        for(int i = 1; i < a.length - 1; i++)
+        {
+            if(a[i].nextLocationRank(a[i+1]) == 1)
+                totalRank += 2;
+            else
+                totalRank-=1;
+        }
+        
+        //then change scheduleScore according to the total rank of schedules
+        if(totalRank >= 5)
+            s.scheduleScore+= 3;
+        if(totalRank <= 1 && totalRank >=-1)
+            s.scheduleScore+=1;
+        if(totalRank <= -1 && totalRank >= -5)
+            s.scheduleScore-=-1;
+        if(totalRank <= -5)
+            s.scheduleScore-=3; 
+    }
+     
+    private int howManyMands(Course[] m)
+    {
+        int mandL = m.length;
+        Course[] mandsTemp = mands;
+        int numMands = 0;
+        for (int i=0; i<mandL; i++){
+            for (int j = 0; j<4; j++)
+                if(m[i].compareTo(mandsTemp[j]) == 1)
+                     numMands++;   
+        }
+        return numMands;
+    }
+    
+    public void sortByTime(Course[] x) 
+    {
+        int n = x.length;
+        for (int pass=1; pass < n; pass++)
+        { 
+            // count how many times
+            // This next loop becomes shorter and shorter
+            for (int i=0; i < n-pass; i++) 
+            {
+                 if (x[i].start_time > x[i+1].start_time) 
+                 {
+                    // exchange elements
+                    Course temp = x[i];  x[i] = x[i+1];  x[i+1] = temp;
+                }
+            }
+        }
+    }
+
+    
+}
+
+
